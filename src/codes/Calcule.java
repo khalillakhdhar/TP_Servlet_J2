79fbc93b83f572e2b65eea45f9ed0eb4289acf44
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-23 11:32:02 +0100

    calcule

--- src/codes/Calcule.java
+++ src/codes/Calcule.java
@@ -54,10 +54,27 @@ public int pgcd()
 {
 	int x=this.a;
 	int y=this.b;
-	
+	while(x!=y)
+	{
+		if(x>y)
+			
+		x=x-y;
+		else
+		y=y-x;
+		
+	}
 	
 	
 	return x;
+
+}
+
+public int ppcm()
+{
+int multiple=this.a*this.b;
+int diviseur=pgcd();
+return multiple/diviseur;
+
 
 }
 }
